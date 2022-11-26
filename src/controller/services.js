5c378d0b49f77211a70234d61dd1357b1fbc0f9d
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2022-11-26 18:46:47 +0700

    test

--- src/controller/services.js
+++ src/controller/services.js
@@ -6,7 +6,7 @@ const episodeHelper = require("../helper/episodeHelper")
 const Services = {
     getOngoing: async (req, res) => {
         const page = req.params.page
-        let url = page === 1 ? "https://komikindo.id/komik-terbaru/" : `https://otakudesu.bid/ongoing-anime/page/${page}/`
+        let url = page === "1" ? "https://komikindo.id/komik-terbaru/" : `https://otakudesu.bid/ongoing-anime/page/${page}/`
         try {
             const response = await services.fetchService(url, res)
             if (response.status === 200) {
