9f0f8a01ab6abd710a937922d22ad0ed0629d432
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2022-11-26 18:30:39 +0700

    test again

--- src/router/route.js
+++ src/router/route.js
@@ -21,7 +21,7 @@ route.get("/", (req, res) => {
 // Get Komik terbaru -Done-
 route.get("/api/v1/komik-terbaru/:page", async (req, res) => {
     const page = req.params.page
-    let url = page === "1" ? `${baseUrl}/komik-terbaru` : `${baseUrl}/komik-terbaru/page/${page}`
+    let url = page === "1" ? `https://komikindo.id/komik-terbaru` : `https://komikindo.id/komik-terbaru/page/${page}`
 
     try {
         const response = await Service.fetchService(url, res)
@@ -71,111 +71,6 @@ route.get("/api/v1/komik-terbaru/:page", async (req, res) => {
     }
 })
 
-// Get Daftar Manga (Japan) -Done-
-route.get('/api/v1/manga/:page/:query?', async (req, res) => {
-    const { page, query } = req.params
-    let url = page === "1" ? `${baseUrl}/daftar-manga/${query ? `?${query}` : ""}` : `${baseUrl}/daftar-manga/page/${page}/${query ? `?${query}` : ""}`
-
-    try {
-        const response = await Service.fetchService(url, res)
-        if (response.status === 200) {
-            const $ = cheerio.load(response.data)
-            let daftar_manga = []
-            let titlePage, thumb, title, score, warna, endpoint
-            titlePage = $(".page-title").text()
-            $(".animepost").each((index, el) => {
-                thumb = $(el).find("img").attr("src")
-                title = $(el).find(".tt").text()
-                score = $(el).find(".rating").text().replace("\n ", "")
-                warna = $(el).find(".warnalabel").text().trim() === "Warna" ? true : false
-                type = $(el).find(".limit > span").attr("class").replace("typeflag ", "")
-                endpoint = $(el).find("a").attr("href").replace(`${baseUrl}/komik/`, "").replace("/", "")
-
-                daftar_manga.push({
-                    title,
-                    thumb,
-                    score,
-                    warna,
-                    type,
-                    endpoint
-                })
-            })
-            return res.status(200).json({
-                status: true,
-                message: "success",
-                titlePage,
-                daftar_manga
-            })
-        }
-        return res.send({
-            message: response.status,
-            daftar_manga
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: false,
-            message: error
-        })
-    }
-})
-
-// Get Daftar Manhua (china) -Done-
-route.get("/api/v1/manhua/:page", async (req, res) => {
-    const page = req.params.page
-    let url = page === "1" ? `${baseUrl}/manhua` : `${baseUrl}/manhua/page/${page}`
-
-    try {
-        const response = await Service.fetchService(url, res)
-        if (response.status === 200) {
-            const $ = cheerio.load(response.data)
-            let daftar_manhua = []
-            let titlePage, thumb, title, score, warna, endpoint
-            titlePage = $(".page-title").text()
-            $(".animepost").each((index, el) => {
-                thumb = $(el).find("img").attr("src")
-                title = $(el).find(".tt").text()
-                score = $(el).find(".rating").text().replace("\n ", "")
-                warna = $(el).find(".warnalabel").text().trim() === "Warna" ? true : false
-                type = $(el).find(".limit > span").attr("class").replace("typeflag ", "")
-                endpoint = $(el).find("a").attr("href").replace(`${baseUrl}/komik/`, "").replace("/", "")
-
-                daftar_manhua.push({
-                    title,
-                    thumb,
-                    score,
-                    warna,
-                    type,
-                    endpoint
-                })
-            })
-            return res.status(200).json({
-                status: true,
-                message: "success",
-                titlePage,
-                daftar_manhua
-            })
-        }
-        return res.send({
-            message: response.status,
-            daftar_manhua
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: false,
-            message: error
-        })
-    }
-})
-
-// Get Daftar Manhwa (korea) -Done-
-route.get("/api/v1/manhwa/:page", async (req, res) => {
-    const page = req.params.page
-    res.send({
-        page
-    })
-})
 
 route.get('*', (req, res) => {
     res.status(404).json({
